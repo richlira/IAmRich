787d6b13e40eaf329dfc2e5e9195037f21fd5b4c
Rich <ricardodelavega@gmail.com>
2022-08-20 20:18:36 -0500

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Ricardo Lira de la Vega on 20/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
